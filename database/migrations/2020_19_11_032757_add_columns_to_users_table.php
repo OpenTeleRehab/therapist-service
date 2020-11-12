d001d4cc5f7e1415b16af60d897fb68aea2ca183
Sreymom Khen <sreymom@web-essentials.co>
2020-11-12 08:16:08 +0700

    [TASK] Implement create therapist backend
    refs TRA-19

---
+++ database/migrations/2020_19_11_032757_add_columns_to_users_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->nullable()->change();
+            $table->string('password')->nullable()->change();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('clinic_id');
+            $table->integer('country_id');
+            $table->integer('limit_patient');
+            $table->integer('language_id')->nullable();
+            $table->integer('profession')->nullable();
+            $table->dropColumn('name');
+            $table->integer('identity')->unique()->nullable();
+
+        });
+
+        DB::statement('ALTER TABLE users CHANGE identity identity INT(9) UNSIGNED ZEROFILL');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name');
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('clinic_id');
+            $table->dropColumn('country_id');
+            $table->dropColumn('language_id');
+            $table->dropColumn('profession');
+            $table->drop('limit_patient');
+        });
+    }
+}
