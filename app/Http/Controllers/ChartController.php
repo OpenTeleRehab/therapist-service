d2cb52ef1ca730fb9770d1dd62402123a997e456
Lina <lina@web-essentials.co>
2021-04-05 11:36:32 +0700

    [TASK] Get country admin data
    refs TRA-11

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class ChartController extends Controller
 {
@@ -23,6 +24,29 @@ class ChartController extends Controller
         return [
             'therapistTotal' => $therapistTotal,
             'therapistsByCountry' => $therapistsByCountry,
+        ];
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function getDataForCountryAdmin(Request $request)
+    {
+        $country_id = $request->get('country_id');
+        $therapistTotal = User::where('country_id', $country_id)->count();
+        $therapistsByClinic = DB::table('users')
+            ->select(DB::raw('
+                clinic_id,
+                COUNT(*) AS total
+            '))
+            ->where('country_id', $country_id)
+            ->groupBy('clinic_id')
+            ->get();
+
+        return [
+            'therapistTotal' => $therapistTotal,
+            'therapistsByClinic' => $therapistsByClinic,
         ];
     }
 }
