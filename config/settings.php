4a038148927ad8dab5fea08b5b52bad158dcd534
Chanthorn Ngeth <chanthorn@web-essentials.asia>
2021-06-22 11:01:49 +0700

    [IMP] Implement api for checking patient number
    refs TRA-628

--- config/settings.php
+++ config/settings.php
@@ -2,5 +2,6 @@
 
 return [
     'date_format' => 'd/m/Y',
-    'therapist_content_limit' => 'therapist_content_limit'
+    'therapist_content_limit' => 'therapist_content_limit',
+    'vn_country_iso' => 'vn',
 ];
