2c146d3208a7add11ab38509cf9fa667d7508165
Seng Sokleap <sokleap@web-essentials.co>
2026-03-16 16:05:47 +0700

    [BUGFIX] Remove user device when delete user
    refs TRA-1271

--- app/Helpers/RocketChatHelper.php
+++ app/Helpers/RocketChatHelper.php
@@ -55,8 +55,7 @@ class RocketChatHelper
             'X-User-Id' => $userId,
         ])->asJson()->post(ROCKET_CHAT_LOGOUT_URL);
         if ($response->successful()) {
-            $result = $response->json();
-            return $result['status'] === 'success';
+            return true;
         }
 
         $response->throw();
